f080f4188e6a1c8eeb2bb77d983e97c136e933f2
陈伟 <chenwei@chenweideiMac.local>
2018-05-20 11:29:48 +0800

    testing 1

--- maventest1/src/main/java/chen/maventest1/HelloWorld.java
+++ maventest1/src/main/java/chen/maventest1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
 	public String sayHello()
 	{
-		return "hello Maven123";
+		return "hello Maven";
 	}
 	public static void main(String[] args) {
 		System.out.print(new HelloWorld().sayHello());

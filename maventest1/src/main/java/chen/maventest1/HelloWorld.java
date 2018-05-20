cc8890b8cd41e61f263b7f3d738b24fa6d547e3a
陈伟 <chenwei@chenweideiMac.local>
2018-05-20 11:50:40 +0800

    merging

--- maventest1/src/main/java/chen/maventest1/HelloWorld.java
+++ maventest1/src/main/java/chen/maventest1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
 	public String sayHello()
 	{
-		return "hello Maven branch1";
+		return "hello Maven";
 	}
 	public static void main(String[] args) {
 		System.out.print(new HelloWorld().sayHello());
